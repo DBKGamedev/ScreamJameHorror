dc1cef135a41125a30e9fab3c752bc2f74d17cc0
DBK Gamedev <DBK_Gamedevs@outlook.com>
2025-10-12 17:17:21 +0530

    interaction-fix

--- scenes/interaction/interactable.gd
+++ scenes/interaction/interactable.gd
@@ -5,5 +5,5 @@ signal interacted
 @export var interact_name : String = ""
 @export var is_interactable : bool = true
 
-var interact : Callable = func():
+func interact() -> void:
 	interacted.emit()
